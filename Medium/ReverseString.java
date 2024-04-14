98ce0742c10fc4e5869b756c01223618187c71e1
Adrián Noya Carro <anoya@Adrians-MacBook-Air.local>
2024-04-14 18:23:52 +0200

    ReverseString

--- Medium/ReverseString.java
+++ Medium/ReverseString.java
@@ -4,40 +4,44 @@ class ReverseString {
         
         // Create a string for the result, a string for the word we construct and a
         // bolean to see if we are in the first word
-        StringBuilder reversed = new StringBuilder();
-        StringBuilder word = new StringBuilder();        
+        StringBuilder reversedSentence = new StringBuilder();
+        StringBuilder word = new StringBuilder();
         boolean firstWord = true;
 
-        for (int i = 0; i <= sentence.length() - 1; i++) {
-            
-            if (sentence.charAt(i) == ' ') {
+        for (int i = 0; i < sentence.length(); i++) {
+            char ch = sentence.charAt(i);
 
-                if (word.length() != 0) {
-
+            if (ch == ' ') {
+                if (word.length() > 0) {  // Solo añade si 'word' no está vacía
                     if (firstWord) {
-                        reversed.append(word);
+                        reversedSentence.append(word);
                         firstWord = false;
-                    } else { 
-                        reversed.insert(0, ' ').insert(0, word);
-                    } 
-                    word.setLength(0);
+                    } else {
+                        reversedSentence.insert(0, ' ').insert(0, word);
+                    }
+                    word.setLength(0);  // Resetear 'word' después de añadir al resultado
                 }
             } else {
-                word.append(sentence.charAt(i));
+                word.append(ch);
             }
         }
 
-        // Adding the last word in we don't have an space
-        if (word.length() > 0) {
-            reversed.insert(0, ' ').insert(0, word);
+        if (word.length() > 0) {  // Para añadir la última palabra si no hay espacio al final
+            if (firstWord) {
+                reversedSentence.append(word);
+            } else {
+                reversedSentence.insert(0, ' ').insert(0, word);
+            }
         }
-        return reversed.toString();
-        }
+
+        return reversedSentence.toString();
+    }
 
     public static void main(String[] args) {
         ReverseString reverse = new ReverseString();
         System.out.println(reverse.reverseString("Hello World"));           // Output: World Hello
         System.out.println(reverse.reverseString("I want emos potatOes"));  // Output: potatOes emos want I
         System.out.println(reverse.reverseString("a good   example"));      // Output: example good a
+        System.out.println(reverse.reverseString("EPY2giL"));               // Output: EPY2giL
     }
 }
