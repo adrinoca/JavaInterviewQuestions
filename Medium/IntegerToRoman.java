e2037070f7d4ed8e306774be290236e23322454d
Adrián Noya Carro <anoya@Adrians-MacBook-Air.local>
2024-04-14 13:07:57 +0200

    IntegerToRoman

--- Medium/IntegerToRoman.java
+++ Medium/IntegerToRoman.java
@@ -2,43 +2,23 @@ import java.util.HashMap;
 
 class Solution {
     public String intToRoman(int num) {
-        // Create HasMap with Roman Numbers
-        HashMap<Character, Integer> romanNumbers = new HashMap<>();
+        // Roman numerals components
+        String[] thousands = {"", "M", "MM", "MMM"};
+        String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
 
-        romanNumbers.put('I', 1);
-        romanNumbers.put('V', 5);
-        romanNumbers.put('X', 10);
-        romanNumbers.put('L', 50);
-        romanNumbers.put('C', 100);
-        romanNumbers.put('D', 500);
-        romanNumbers.put('M', 1000);
-
-        // Variables
-        char numer = 'I';
-
-        // Iterate over the string of Roman Numbers
-        for (int i = 0; i < s.length(); i++) {
-
-            char currentChar = s.charAt(i);
-            int currentValue = romanNumbers.get(currentChar);
-
-            // If the previous value is smaller than the current value we
-            // have to substract to times it from the total sum.
-            if (prevValue < currentValue) {
-                sum -= 2 * prevValue;
-            }
-
-            sum += currentValue;
-            prevValue = currentValue;
-
-        }
-        return sum;
+        // Build the Roman numeral string from the highest place values to the lowest
+        return thousands[num / 1000] +
+               hundreds[(num % 1000) / 100] +
+               tens[(num % 100) / 10] +
+               ones[num % 10];
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.intToRoman(3));    // Output: III
-        System.out.println(solution.intToRoman(58));     // Output: LVIII
-        System.out.println(solution.intToRoman(1994));     // Output: MCMXCIV
+        System.out.println(solution.intToRoman(3));    // Output: "III"
+        System.out.println(solution.intToRoman(58));     // Output: "LVIII"
+        System.out.println(solution.intToRoman(1994));     // Output: "MCMXCIV"
     }
 }
